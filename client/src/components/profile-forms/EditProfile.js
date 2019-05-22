f634e212f76e57a0da5aafa3ca4c083d095d5eaf
Brandon Redshirt <brandon.redshirt@gmail.com>
2019-05-22 04:28:38 -0600

    More console warning clean up

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -48,7 +48,7 @@ const EditProfile = ({
             instagram:
                 loading || !profile.social ? '' : profile.social.instagram
         });
-    }, [loading, getCurrentProfile, profile]);
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
